3be2156379a9cd36e39484b6af022625be3b4b49
yjcs1990 <yjcs1990@163.com>
2017-05-17 15:28:42 +0800

    Include some head files

--- include/MvrMutex.h
+++ include/MvrMutex.h
@@ -6,3 +6,13 @@
  > Create Time  : 2017年05月10日
  > Modify Time  : 2017年05月10日
 ***************************************************************************************************/
+#ifndef MVRMUTEX_H
+#define MVRMUTEX_H
+
+#if !ifdefined(WIN32) || defined(MINGW)
+#include <pthread.h>
+#endif
+#include <string>
+#include <mvriaTypedefs.h>
+
+#endif  // MVRMUTEX_H
