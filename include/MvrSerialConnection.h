4b9a37e77792e75c5dafe274e463f1a6b2f4c0da
yjcs1990 <yjcs1990@163.com>
2017-05-17 14:46:32 +0800

    serial connection defination

---
+++ include/MvrSerialConnection.h
@@ -0,0 +1,8 @@
+/*************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrSerialConnection.h
+ > Description  : Some pre-defined type
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月10日
+**************************************************/
