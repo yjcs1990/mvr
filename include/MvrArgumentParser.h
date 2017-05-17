170d7c74090d5f038238a38c33485aa6df53d3d0
yjcs1990 <yjcs1990@163.com>
2017-05-17 17:50:04 +0800

    #Include head files

--- include/MvrArgumentParser.h
+++ include/MvrArgumentParser.h
@@ -6,3 +6,11 @@
  > Create Time  : 2017年05月10日
  > Modify Time  : 2017年05月17日
 ***************************************************************************************************/
+
+#ifndef MVRARGUMENTPARSER_H
+#define MVRARGUMENTPARSER_H
+
+#include "mvriaTypedefs.h"
+#include "MvrArgumentBuiler.h"
+/// TODO
+#endif  // MVRARGUMENTPARSER_H
