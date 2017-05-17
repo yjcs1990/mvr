50f2d4a9d9d8d554c89a7ba69407013448215f12
ycjs1990 <yjcs1990@163.com>
2017-05-17 15:17:11 +0800

    Add some head files

--- include/mvriaUtil.h
+++ include/mvriaUtil.h
@@ -10,5 +10,30 @@
 #ifndef MVRIAUTIL_H
 #define MVRIAUTIL_H
 
-#
+#define _GUN_SOURCE 1
+#include <string>
+// #define _XOPEN_SOURCE 500
+#include <list>
+#include <map>
+#include <math.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <float.h>
+#include <vector>
+
+#if defined(_WIN32) || defined(WIN32)
+#include <sys/timeb.h>
+#include <sys/stat.h>
+#else
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <strings.h>
+#endif // ifndef win32
+#include <time.h>
+#include "mvriaTypedefs.h"
+#include "ArLog.h"
 #endif  // MVRIAUTIL_H
