411898bd4d66c71d588e169297de2cc7c28d60d7
yjcs1990 <yjcs1990@163.com>
2017-05-17 16:30:56 +0800

    Add relative classes

--- include/MvrFunctor.h
+++ include/MvrFunctor.h
@@ -45,4 +45,108 @@ protected:
   std::string myName;
 };
 
+/// Base class for functors with 1 parameter 
+/**
+ *  This is the base class for functors with 1 parameter. Code that has a
+ *  reference to a functor that takes 1 parameter should use this class
+ *  name. This allows the code to know how to invoke the functor without
+ *  knowing which class the member function is in.
+
+ *  For an overall description of functors, see MvrFunctor.
+*/
+template<class P1>
+class MvrFunctor1 : public MvrFunctor
+{
+public:
+
+  /// Destructor
+  virtual ~MvrFunctor1() {}
+
+  /// Invokes the functor
+  virtual void invoke(void) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+  */
+  virtual void invoke(P1 p1) = 0;
+};
+
+/// Base class for functors with 2 parameters
+/**
+   This is the base class for functors with 2 parameters. Code that has a
+   reference to a functor that takes 2 parameters should use this class
+   name. This allows the code to know how to invoke the functor without
+   knowing which class the member function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class P1, class P2>
+class MvrFunctor2 : public MvrFunctor1<P1>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrFunctor2() {}
+
+  /// Invokes the functor
+  virtual void invoke(void) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+  */
+  virtual void invoke(P1 p1) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual void invoke(P1 p1, P2 p2) = 0;
+};
+
+/// Base class for functors with 3 parameters
+/**
+   This is the base class for functors with 3 parameters. Code that has a
+   reference to a functor that takes 3 parameters should use this class
+   name. This allows the code to know how to invoke the functor without
+   knowing which class the member function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class P1, class P2, class P3>
+class MvrFunctor3 : public MvrFunctor2<P1, P2>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrFunctor3() {}
+
+  /// Invokes the functor
+  virtual void invoke(void) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+  */
+  virtual void invoke(P1 p1) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual void invoke(P1 p1, P2 p2) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+  */
+  virtual void invoke(P1 p1, P2 p2, P3 p3) = 0;
+};
+
+
 #endif  // MVRFUNCTOR_H
