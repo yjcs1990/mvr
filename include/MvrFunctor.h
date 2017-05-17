7e172285ff624d1b290e590bdc57b71964a0a0e6
yjcs1990 <yjcs1990@163.com>
2017-05-17 16:24:13 +0800

    Init MvrFunctor head file

---
+++ include/MvrFunctor.h
@@ -0,0 +1,9 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrFunctor.h
+ > Description  : An object which allows storing a generalized reference 
+                  to a method with an object instance to call later (used for callback functions)
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月10日
+***************************************************************************************************/
