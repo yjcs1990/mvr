6625c712cdbb51b40a375feec31f2dfe1c1a392b
yjcs1990 <yjcs1990@163.com>
2017-05-17 17:51:18 +0800

    This class is to build arguments for things that require argc and argv

---
+++ include/MvrArgumentBuiler.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrArgumentBuilder.h
+ > Description  : Base class for device connections
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月17日
+***************************************************************************************************/
+
+#ifndef MVRARGUMENTBUILDER_H
+#define MVRARGUMENTBUILDER_H
+
+
+
+#endif  // MVRARGUMENTBUILDER_H
