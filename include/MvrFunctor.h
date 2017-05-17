659216d5f129779b2ea1b30ec9bb356bb7e2d9aa
yjcs1990 <yjcs1990@163.com>
2017-05-17 16:36:49 +0800

    Add a functor with return value class

--- include/MvrFunctor.h
+++ include/MvrFunctor.h
@@ -47,12 +47,12 @@ protected:
 
 /// Base class for functors with 1 parameter 
 /**
- *  This is the base class for functors with 1 parameter. Code that has a
- *  reference to a functor that takes 1 parameter should use this class
- *  name. This allows the code to know how to invoke the functor without
- *  knowing which class the member function is in.
+   This is the base class for functors with 1 parameter. Code that has a
+   reference to a functor that takes 1 parameter should use this class
+   name. This allows the code to know how to invoke the functor without
+   knowing which class the member function is in.
 
- *  For an overall description of functors, see MvrFunctor.
+   For an overall description of functors, see MvrFunctor.
 */
 template<class P1>
 class MvrFunctor1 : public MvrFunctor
@@ -148,5 +148,308 @@ public:
   virtual void invoke(P1 p1, P2 p2, P3 p3) = 0;
 };
 
+/// Base class for functors with 4 parameters
+/**
+   This is the base class for functors with 4 parameters. Code that has a
+   reference to a functor that takes 4 parameters should use this class
+   name. This allows the code to know how to invoke the functor without
+   knowing which class the member function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class P1, class P2, class P3, class P4, class P5>
+class MvrFunctor5 : public MvrFunctor4<P1, P2, P3, P4>
+{
+public:
 
+  /// Destructor
+  virtual ~MvrFunctor5() {}
+
+  /// Invokes the functor
+  virtual void invoke(void) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+  */
+  virtual void invoke(P1 p1) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual void invoke(P1 p1, P2 p2) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+  */
+  virtual void invoke(P1 p1, P2 p2, P3 p3) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+     @param p4 fourth parameter
+ */
+  virtual void invoke(P1 p1, P2 p2, P3 p3, P4 p4) = 0;
+
+  /// Invokes the functor
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+     @param p4 fourth parameter
+     @param p5 fifth parameter
+ */
+  virtual void invoke(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5) = 0;
+};
+
+/// Base class for functors with a return value
+/**
+   This is the base class for functors with a return value. Code that has a
+   reference to a functor that returns a value should use this class
+   name. This allows the code to know how to invoke the functor without
+   knowing which class the member function is in.
+   
+   For an overall description of functors, see MvrFunctor.     
+*/
+template<class Ret>
+class MvrRetFunctor : public MvrFunctor
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor() {}
+
+  /// Invokes the functor
+  virtual void invoke(void) {invokeR();}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+};
+
+/// Base class for functors with a return value with 1 parameter
+/**
+   This is the base class for functors with a return value and take 1
+   parameter. Code that has a reference to a functor that returns a value
+   and takes 1 parameter should use this class name. This allows the code
+   to know how to invoke the functor without knowing which class the member
+   function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class Ret, class P1>
+class MvrRetFunctor1 : public MvrRetFunctor<Ret>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor1() {}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+  */
+  virtual Ret invokeR(P1 p1) = 0;
+};
+
+/// Base class for functors with a return value with 2 parameters
+/**
+   This is the base class for functors with a return value and take 2
+   parameters. Code that has a reference to a functor that returns a value
+   and takes 2 parameters should use this class name. This allows the code
+   to know how to invoke the functor without knowing which class the member
+   function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class Ret, class P1, class P2>
+class MvrRetFunctor2 : public MvrRetFunctor1<Ret, P1>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor2() {}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+  */
+  virtual Ret invokeR(P1 p1) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2) = 0;
+};
+
+/// Base class for functors with a return value with 3 parameters
+/**
+   This is the base class for functors with a return value and take 3
+   parameters. Code that has a reference to a functor that returns a value
+   and takes 3 parameters should use this class name. This allows the code
+   to know how to invoke the functor without knowing which class the member
+   function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class Ret, class P1, class P2, class P3>
+class MvrRetFunctor3 : public MvrRetFunctor2<Ret, P1, P2>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor3() {}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+  */
+  virtual Ret invokeR(P1 p1) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3) = 0;
+};
+
+/// Base class for functors with a return value with 4 parameters
+/**
+   This is the base class for functors with a return value and take 4
+   parameters. Code that has a reference to a functor that returns a value
+   and takes 4 parameters should use this class name. This allows the code
+   to know how to invoke the functor without knowing which class the member
+   function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class Ret, class P1, class P2, class P3, class P4>
+class MvrRetFunctor4 : public MvrRetFunctor3<Ret, P1, P2, P3>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor4() {}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+  */
+  virtual Ret invokeR(P1 p1) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+     @param p4 fourth parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3, P4 p4) = 0;
+};
+
+/// Base class for functors with a return value with 5 parameters
+/**
+   This is the base class for functors with a return value and take 5
+   parameters. Code that has a reference to a functor that returns a value
+   and takes 5 parameters should use this class name. This allows the code
+   to know how to invoke the functor without knowing which class the member
+   function is in.
+   
+   For an overall description of functors, see MvrFunctor.
+*/
+template<class Ret, class P1, class P2, class P3, class P4, class P5>
+class MvrRetFunctor5 : public MvrRetFunctor4<Ret, P1, P2, P3, P4>
+{
+public:
+
+  /// Destructor
+  virtual ~MvrRetFunctor5() {}
+
+  /// Invokes the functor with return value
+  virtual Ret invokeR(void) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+  */
+  virtual Ret invokeR(P1 p1) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+     @param p4 fourth parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3, P4 p4) = 0;
+
+  /// Invokes the functor with return value
+  /**
+     @param p1 first parameter
+     @param p2 second parameter
+     @param p3 third parameter
+     @param p4 fourth parameter
+     @param p5 fifth parameter
+  */
+  virtual Ret invokeR(P1 p1, P2 p2, P3 p3, P4 p4, P5 p5) = 0;
+};
 #endif  // MVRFUNCTOR_H
