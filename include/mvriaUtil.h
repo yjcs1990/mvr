cf3e6b5c8a0e2b82b9f114bc1632793a06e04c29
ycjs1990 <yjcs1990@163.com>
2017-05-17 15:07:03 +0800

    Some Utility classes and functions

---
+++ include/mvriaUtil.h
@@ -0,0 +1,14 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : mvriaUtil.h
+ > Description  : Some Utility classes and functions
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月10日
+***************************************************************************************************/
+
+#ifndef MVRIAUTIL_H
+#define MVRIAUTIL_H
+
+#
+#endif  // MVRIAUTIL_H
