74e0a643d5d3eb099d290a7a2e1fba0575fa776e
yjcs1990 <yjcs1990@163.com>
2017-05-17 17:48:25 +0800

    #Init a new head file

---
+++ include/MvrArgumentParser.h
@@ -0,0 +1,8 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrArgumentParser.h
+ > Description  : Base class for device connections
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月17日
+***************************************************************************************************/
