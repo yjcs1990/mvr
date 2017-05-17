b0a87ca1d059654d64ad0e5e7fa555bf36a9d3df
yjcs1990 <yjcs1990@163.com>
2017-05-17 16:16:44 +0800

    Cmake files

---
+++ build/CMakeFiles/MVRTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/MVRTest.dir/test/test.cpp.o"
+  "MVRTest.pdb"
+  "MVRTest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/MVRTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
