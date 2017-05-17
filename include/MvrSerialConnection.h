c4d45f896f396ec4b42d2f65a5a9694e3af58f19
yjcs1990 <yjcs1990@163.com>
2017-05-17 14:50:17 +0800

    some modification

--- include/MvrSerialConnection.h
+++ include/MvrSerialConnection.h
@@ -1,8 +1,8 @@
-/*************************************************
+/**************************************************************************************************
  > Project Name : MVR - mobile vacuum robot
  > File Name    : MvrSerialConnection.h
- > Description  : Some pre-defined type
+ > Description  : Utility for connecting to and communicating with devices through a serial port
  > Author       : Yu Jie
  > Create Time  : 2017年05月10日
  > Modify Time  : 2017年05月10日
-**************************************************/
+***************************************************************************************************/
