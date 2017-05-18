a14e62e5c29d7284763437414d8c27a28b2b9673
yjcs1990 <yjcs1990@163.com>
2017-05-18 10:59:45 +0800

    #New head file

---
+++ include/MvrBasePacket.h
@@ -0,0 +1,8 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrBasePacket.h
+ > Description  : Base packet class
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月17日
+***************************************************************************************************/
