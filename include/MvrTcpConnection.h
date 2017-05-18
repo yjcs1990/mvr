a6c8342d6a6056b7526ad2dca851bcf36889933b
yjcs1990 <yjcs1990@163.com>
2017-05-18 14:54:02 +0800

    #add head file

---
+++ include/MvrTcpConnection.h
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrTcpConnection.h
+ > Description  : For connecting to a device through a TCP network socket
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月18日
+ > Modify Time  : 2017年05月18日
+***************************************************************************************************/
+#ifndef MVRTCPCONNECTION_H
+#define MVRTCPCONNECTION_H
+
+#include "MvrDeviceConnection.h"
+#include <string>
+
+#include "mvriaTypedefs.h"
+#include "MvrSocket.h"
+
+#endif  //MVRTCPCONNECTION_H
