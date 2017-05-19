351c6a2ad1caf6bcf2efc4acfa75adf5a53d9743
yjcs1990 <yjcs1990@163.com>
2017-05-19 11:24:19 +0800

    #init new head file

---
+++ include/MvrRobotConfigPacketReader.h
@@ -0,0 +1,8 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrRobotConnector.h
+ > Description  : Connect to robot or simulator based on program command line parameters.
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月10日
+ > Modify Time  : 2017年05月10日
+***************************************************************************************************/
