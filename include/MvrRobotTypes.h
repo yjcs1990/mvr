63be4d993b14ea1967c1295b5fbd11bb65d5c850
yjcs1990 <yjcs1990@163.com>
2017-05-19 14:55:50 +0800

    #init new class

---
+++ include/MvrRobotTypes.h
@@ -0,0 +1,15 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrRobotTypes.h
+ > Description  : Include internal Robot parameter class
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月19日
+ > Modify Time  : 2017年05月19日
+***************************************************************************************************/
+#ifndef MVRROBOTTYPES_H
+#define MVRROBOTTYPES_H
+
+#include "mvriaUtil.h"
+#include "MvrRobotParams.h"
+
+#endif  // MVRROBOTTYPES_H
