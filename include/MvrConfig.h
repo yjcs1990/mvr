db756163ae2324d3946bafb6d68e1528970ec1c1
yjcs1990 <yjcs1990@163.com>
2017-05-20 15:36:58 +0800

    #init new head file

---
+++ include/MvrConfig.h
@@ -0,0 +1,8 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrConfig.h
+ > Description  : Stores configuration information which may be read to and from files or other sources
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月20日
+ > Modify Time  : 2017年05月20日
+***************************************************************************************************/
