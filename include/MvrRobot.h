88963f1ae9b6e68697328fa8b1a0da7904cb31d2
yjcs1990 <yjcs1990@163.com>
2017-05-22 08:10:43 +0800

    #init the new head file

---
+++ include/MvrRobot.h
@@ -0,0 +1,19 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrRobot.h
+ > Description  : This class will read a config packet from the robot.
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月22日
+ > Modify Time  : 2017年05月22日
+***************************************************************************************************/
+#ifndef MVRROBOT_H
+#define MVRROBOT_H
+
+#include "mvriaTypedefs.h"
+#include "MvrRobotPacketSender.h"
+#include "MvrRobotPacketReceiver.h"
+#include "MvrFunctor.h"
+#include "MvrSyncTask.h"
+#include 
+
+#endif  // MVRROBOT_H
