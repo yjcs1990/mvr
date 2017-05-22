e907c5f12b682ce694fd8394836b8fdc00de536f
yjcs1990 <yjcs1990@163.com>
2017-05-22 15:04:46 +0800

    #add new functions into class

--- include/MvrASyncTask.h
+++ include/MvrASyncTask.h
@@ -13,6 +13,60 @@
 #include "MvrFunctor.h"
 #include "MvrThread.h"
 
-/// TODO
+/// @ingroup UtilityClasses
+class MvrASyncTask : public MvrThread
+{
+public:
+  /// Constructor
+  MVREXPORT MvrASyncTask();
+  /// Destructor
+  MVREXPORT virtual ~MvrASyncTask();
+
+  /// The main run loop
+  /*
+     Override this function and put your taskes run loop here. Check the
+     value of getRunning() periodicly in your loop. If the value
+     is false, the loop should exit and runThread() should return.
+     The argument and return value are specific to the platform thread implementation, and 
+     can be ignored.
+     @swignote In the wrapper libraries, this method takes no arguments and has no return value.
+  */
+  MVREXPORT virtual void * runThread(void *arg) = 0;
+
+  /// Run without creating a new thread
+/**
+   This will run the the MvrASyncTask without creating a new
+   thread to run it in. It performs the needed setup then calls runThread() 
+   directly instead of letting the system threading system do it in a new thread.
+*/
+  virtual void run(void) { runInThisThread(); }
+  
+  /// Run in its own thread
+  virtual void runAsync(void) { create(); }
+
+  // reimplemented here just so its easier to see in the docs
+  /// Stop the thread
+  virtual void stopRunning(void) {myRunning=false;}
+
+  /// Create the task and start it going
+  MVREXPORT virtual int create(bool joinable=true, bool lowerPriority=true);
+
+  /** Internal function used with system threading system to run the new thread.
+      In general, use run() or runAsync() instead.
+      @internal
+  */
+  MVREXPORT virtual void * runInThisThread(void *arg=0);
+
+  /// Gets a string that describes what the thread is doing, or NULL if it
+  /// doesn't know. Override this in your subclass to return a status
+  /// string. This can be used for debugging or UI display.
+  virtual const char *getThreadActivity(void) { return NULL; }
+private:
+  // Hide regular Thread::Create
+  virtual int create(MvrFunctor * func, bool joinable=true, bool lowerPriority=true) 
+    { return(false); }
+    
+  MvrRetFunctor1C<void*, MvrASyncTask, void*> myFunc;
+};
 
 #endif  // MVRASYNCTASK_H
