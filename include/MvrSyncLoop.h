3c16d87a79c0eed3e1c195d266837766c16bdf26
yjcs1990 <yjcs1990@163.com>
2017-05-22 15:04:33 +0800

    #add new functions into class

--- include/MvrSyncLoop.h
+++ include/MvrSyncLoop.h
@@ -13,5 +13,23 @@
 #include "MvrASyncTask.h"
 #include "MvrSyncTask.h"
 
-/// TODO
+class MvrRobot;
+
+class MvrSyncLoop : public MvrASyncTask
+{
+public:
+  MVREXPORT MvrSyncLoop();
+  MVREXPORT virtual ~MvrSyncLoop();
+
+  MVREXPORT void setRobot(MvrRobot *robot);
+
+  MVREXPORT void stopRunIfNotConnected(bool stopRun);
+  MVREXPORT virtual void *runThread(void *arg);
+
+  MVREXPORT virtual const char *getThreadActivity(void);
+protected:
+  bool myStopRunIfNotConnected;
+  MvrRobot *myRobot;
+  bool myInRun;
+};
 #endif  // MVRSYNCLOOP_H
