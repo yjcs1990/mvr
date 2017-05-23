21a07cda6f58038ebe1b0893fa7d952ad0d4ea19
yjcs1990 <yjcs1990@163.com>
2017-05-23 21:06:52 +0800

    #add new head file

---
+++ include/MvrSonarDevice.h
@@ -0,0 +1,18 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrSonarDevice.h
+ > Description  : Keep track of recent sonar readings from a robot as an MvrRangeDevice
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月23日
+ > Modify Time  : 2017年05月23日
+***************************************************************************************************/
+#ifndef MVRSONARDEVICE_H
+#define MVRSONARDEVICE_H
+
+#include "mvriaTypedefs.h"
+#include "MvrRangeDevice.h"
+#include "MvrFunctor.h"
+
+#include "MvrRobot.h"
+
+#endif  // MVRSONARDEVICE_H
