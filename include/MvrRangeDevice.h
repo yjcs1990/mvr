f534fbc319321449b940f3ebb97c3a4a199900f6
yjcs1990 <yjcs1990@163.com>
2017-05-23 21:07:41 +0800

    #add new head file

---
+++ include/MvrRangeDevice.h
@@ -0,0 +1,19 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrRangeDevice.h
+ > Description  : The base class for all sensing devices which return range
+                  information from the device (mounted on a robot) to an object in
+                  the environment.
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月23日
+ > Modify Time  : 2017年05月23日
+***************************************************************************************************/
+#ifndef MVRRANGEDEVICE_H
+#define MVRRANGEDEVICE_H
+
+#include "mvriaTypedefs.h"
+#include "MvrRangeBuffer.h"
+#include "MvrSensorReading.h"
+#include "MvrDrawingData.h"
+
+#endif  // MVRRANGEDEVICE_H
