9216f0d714c77060dd8e7b84a602b5ae4040846c
yjcs1990 <yjcs1990@163.com>
2017-05-24 11:51:24 +0800

    #init new head file

---
+++ include/MvrAction.h
@@ -0,0 +1,16 @@
+/**************************************************************************************************
+ > Project Name : MVR - mobile vacuum robot
+ > File Name    : MvrAction.h
+ > Description  : This class is the Base class for actions
+ > Author       : Yu Jie
+ > Create Time  : 2017年05月24日
+ > Modify Time  : 2017年05月24日
+***************************************************************************************************/
+#ifndef MVRACTION_H
+#define MVRACTION_H
+
+#include "mvriaTypedefs.h"
+#include "MvrArg.h"
+/// TODO
+
+#endif  // MVRACTION_H
