81aa0032f5ac50c78924f568bdce7732fe629f7c
yjcs1990 <yjcs1990@163.com>
2017-05-27 15:17:56 +0800

    #modify workspace file

--- src/MvrArgumentBuilder.cpp
+++ src/MvrArgumentBuilder.cpp
@@ -4,7 +4,7 @@
  > Description  : Base class for device connections
  > Author       : Yu Jie
  > Create Time  : 2017年04月10日
- > Modify Time  : 2017年05月17日
+ > Modif y Time  : 2017年05月17日
 ***************************************************************************************************/
 #include "MvrExport.h"
 #include "mvriaOSDef.h"
@@ -60,7 +60,7 @@ MVREXPORT MvrArgumentBuilder::MvrArgumentBuilder(const MvrArgumentBuilder &build
   myArgvLen = builder.getArgvLen();
   myOrigArgc = myArgc;
   myArgv = new char *[myArgvLen];
-  for(i=0; i<myArgc;i++)
+  for (i=0; i<myArgc;i++)
     myArgv[i] = cppstrdup(builder.getArg(i));
   myIsQuiet = builder.myIsQuiet;
   myExtraSpace = builder.MyExtraSpace;
@@ -70,14 +70,14 @@ MVREXPORT MvrArgumentBuilder::MvrArgumentBuilder(const MvrArgumentBuilder &build
 
 MVREXPORT MvrArgumentBuilder &MvrArgumentBuilder::operator=(const MvrArgumentBuilder &builder)
 {
-  if(this != &builder)
+  if (this != &builder)
   {
     size_t i=0;
 
     /// Delete old stuff ...
-    if(myOrigArgc > 0)
+    if (myOrigArgc > 0)
     {
-      for(i=0; i<myOrigArgc; ++i)
+      for (i=0; i<myOrigArgc; ++i)
         delete [] myArgv[i];
     }
     delete [] myArgv;
@@ -104,9 +104,9 @@ MVREXPORT MvrArgumentBuilder &MvrArgumentBuilder::operator=(const MvrArgumentBui
 MVREXPORT MvrArgumentBuilder::~MvrArgumentBuilder()
 {
   size_t i;
-  if(myOrigArgc > 0)
+  if (myOrigArgc > 0)
   {
-    for(i=0; i<myOrigArgc; ++i)
+    for (i=0; i<myOrigArgc; ++i)
       delete [] myArgv[i];
   }
   delete [] myArgv;
@@ -117,13 +117,13 @@ MVREXPORT void MvrArgumentBuilder::removeArg(size_t which, bool isRebuildFullStr
   size_t i;
   char *temp;
 
-  if(which < 0)
+  if (which < 0)
   {
     MvrLog::log(MvrLog::Terse, "MvrArgumentBuilder::removeArg: cannot remove arg at negative index (%i)",
                 which);
     return;
   }
-  if(which>myArgc-1)
+  if (which>myArgc-1)
   {
     MvrLog::log(MvrLog::Terse, "MvrArgumentBuilder::removeArg: %d is greater than the number of arguments which is %d",
                 which, myArgc)
@@ -132,7 +132,7 @@ MVREXPORT void MvrArgumentBuilder::removeArg(size_t which, bool isRebuildFullStr
 
   temp = myArgv[which];
   //delete [] myArgv[which]
-  for(i=which; i<myArgc-1; i++)
+  for (i=which; i<myArgc-1; i++)
     myArgv[i] = myArgv[i+1];
   /// delete the one off the end
   myArgc = -1;
@@ -140,9 +140,9 @@ MVREXPORT void MvrArgumentBuilder::removeArg(size_t which, bool isRebuildFullStr
   myArgv[i] = temp;
 
   /// Note: It seems that compressQuoted calls removeArg and depends on it not
-  /// changing the full string. Therefore, the parameter was added so that 
-  /// the desired behavior could be specified by the caller
-  if(isRebuildFullString)
+  /// changing the full string. Therefor e, the parameter was added so that 
+  /// the desired behavior could be specif ied by the caller
+  if (isRebuildFullString)
     rebuildFullString();
 }
 
@@ -158,10 +158,10 @@ MVREXPORT void MvrArgumentBuilder::add(const char *str, ...)
 
 bool MvrArgumentBuilder::isSpace(char c)
 {
-  if((!myIgnoreNormalSpaces) && (isSpace(c))){
+  if ((!myIgnoreNormalSpaces) && (isSpace(c))){
     return true;
   }
-  else if((myExtraSpace != '\0') && (c == myExtraSpace)){
+  else if ((myExtraSpace != '\0') && (c == myExtraSpace)){
     return true;
   }
   return false;
@@ -169,15 +169,15 @@ bool MvrArgumentBuilder::isSpace(char c)
 
 bool MvrArgumentBuilder::isStartArg(const char *buf, int len, int index, int *endArgFlagsOut)
 {
-  if(index<len){
-    if(!isSpace(buf[index])){
-      if((myIsPreCompressQuotes) && (buf[index] == '\"')){
-        if(endArgFlagsOut != NULL){
+  if (index<len){
+    if (!isSpace(buf[index])){
+      if ((myIsPreCompressQuotes) && (buf[index] == '\"')){
+        if (endArgFlagsOut != NULL){
           *endArgFlagsOut = ANY_SPACE | QUOTE;
         }
       }
       else {  //not precompressed quote
-        if(endArgFlagsOut != NULL){
+        if (endArgFlagsOut != NULL){
           /// This is set to ANY_SPACE to preserve the original behavior ... i.e. space
           /// charactre "matches" with myExtraSpace
           *endArgFlagsOut = ANY_SPACE;
@@ -191,11 +191,11 @@ bool MvrArgumentBuilder::isStartArg(const char *buf, int len, int index, int *en
 
 bool MvrArgumentBuilder::isEndArg(const char *buf, int len, int &index, int endArgFlags)
 {
-  if(index >= len)
+  if (index >= len)
     return true;
-  if((endArgFlags &QUOTE) != 0){
-    if(buf[index] == '\"'){
-      if((index+1 >= len) || (buf[index+1] == '\0')){
+  if ((endArgFlags &QUOTE) != 0){
+    if (buf[index] == '\"'){
+      if ((index+1 >= len) || (buf[index+1] == '\0')){
         /// Quoteis not EOF
         index ++;
         return true;
@@ -203,7 +203,7 @@ bool MvrArgumentBuilder::isEndArg(const char *buf, int len, int &index, int endA
     } // end if quote found
   } // end if need to find quote
   else { // just looking for a space
-    if(isSpace(buf[index])){
+    if (isSpace(buf[index])){
       return true;
     }
   }
@@ -232,7 +232,7 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
 
   if (position < 0 || (size_t)position > myArgc)
     addAtEnd = true;
-  else
+  else 
     AddAtEnd = false;
 
   strncpy(buf, str, sizeof(buf));
@@ -240,14 +240,14 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
 
   /// can do whatever you want with the buf now
   /// first we advance to non-space
-  for(i=0; i<len; ++i)
+  for (i=0; i<len; ++i)
   {
-    if(!isSpace(buf[i]))
+    if (!isSpace(buf[i]))
       break;
   }
-  if(i==len)
+  if (i==len)
   {
-    if(!myIsQuiet){
+    if (!myIsQuiet){
       MvrLog::log(MvrLog::Verbose, "All white space add for argument builder.");
     }
     return;
@@ -256,41 +256,41 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
   int endArgFlags = ANY_SPACE;
 
   /// Walk througth the line until we get the end of the buffer..
-  for(curArgStartIndex=i; ; ++i)
+  for (curArgStartIndex=i; ; ++i)
   {
     /// Remove the slash of esaceped spaces. This is primarily done to hanle command
     /// line argumetns (especially on linux)
-    if(!myIsPreCompressQuotes && (buf[i] == '\\') && (i+1 < len) && (buf[i+1] == ' ') &&
+    if (!myIsPreCompressQuotes && (buf[i] == '\\') && (i+1 < len) && (buf[i+1] == ' ') &&
         ((i==0) || (buf[i-1] != '\\')))
     {
-      for(j=i; j<len && j!='\0'; j++)
+      for (j=i; j<len && j!='\0'; j++)
       {
         buf[j] = buf[j+1];
       }
       --len;
     }
-    /// If we're not in the middle of an argument, then determine whether the
+    /// if we're not in the middle of an argument, then determine whether the
     /// current buffer position marks the start of one.
-    else if((!isArgInProgress) && (i<len) && (buf[i] !='\0') && (isStartArg(buf, len,i, &endArgFlags))) 
+    else if ((!isArgInProgress) && (i<len) && (buf[i] !='\0') && (isStartArg(buf, len,i, &endArgFlags))) 
     {
       curArgStartIndex = i;
       isArgInProgress = true;
     }
-    /// If we are in the middle of argument, then determine whether the current
+    /// if we are in the middle of argument, then determine whether the current
     /// buffer position marks the end of it.
-    else if(isArgInProgress && ((i==len) || (buf[i]=='\0') || (isEndArg(buf, len, i, endArgFlags))))
+    else if (isArgInProgress && ((i==len) || (buf[i]=='\0') || (isEndArg(buf, len, i, endArgFlags))))
     {
       /// See if we have room in our argvLen
-      if(myArgc+1 >= myArgvLen)
+      if (myArgc+1 >= myArgvLen)
       {
         MvrLog::log(MvrLog::Terse, "MvrArgumentBuilder::Add: could not add argument since argc 
                     (%u) has grown beyond the argv given in the constructor (%u)", myArgc, myArgvLen);
       }
       else // room in arg araray
       {
-        /// If we're adding at the end just put it there, also put it 
+        /// if we're adding at the end just put it there, also put it 
         /// at the end if its too far out
-        if(addAtEnd)
+        if (addAtEnd)
         {
           myArgv[myArgc] = new char[i-curArgStartIndex + 1];
           strncpy(myArgv[myArgc], &buf[curArgStartIndex], i-curArgStartIndex]);
@@ -298,9 +298,9 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
 
           /// add to our full string
           /// if its not our fires aadd a space
-          if(!myFirstAdd && myExtraSpace=='\0')
+          if (!myFirstAdd && myExtraSpace=='\0')
             myFullString += " ";
-          else if(!myFirstAdd)
+          else if (!myFirstAdd)
             myFullString += myExtraSpace;
             
           myFullString += myArgv[myArgc];
@@ -310,11 +310,11 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
           myOrigArgc = myArgc;
         }
         /// otherwise stick it where we wanted it if we can or just insert arg
-        /// at specified position
-        else
+        /// at specif ied position
+        else 
         {
           /// first move things down
-          for(k=myArgc+1; k>(size_t)position; k--)
+          for (k=myArgc+1; k>(size_t)position; k--)
           {
             myArgv[k] = myArgv[k-1];
           }
@@ -333,7 +333,7 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
       isArgInProgress = false;
       endArgFlags = ANY_SPACE;
     }
-    if(i==len || buf[i] == '\0')
+    if (i==len || buf[i] == '\0')
       break;
   }
 }
@@ -359,17 +359,17 @@ MVREXPORT void MvrArgumentBuilder::internalAdd(const char *str, int position)
 MVREXPORT void MvrArgumentBuilder::addStrings(int argc, char **argv, int position)
 {
   int i;
-  if(position < 0)
+  if (position < 0)
   {
     /// Don't try to use incremental positions, since the sequence would be out
     /// of order, just keep using the same special "at-end" meaning of negative position
     /// value
-    for(i=0; i<argc; i++)
+    for (i=0; i<argc; i++)
       internalAdd(argv[i], position);
   }
-  else
+  else 
   {
-    for(i=0; i<argc; i++)
+    for (i=0; i<argc; i++)
       internalAdd(argv[i], position+i);
   }
 }
@@ -384,17 +384,17 @@ MVREXPORT void MvrArgumentBuilder::addStrings(int argc, char **argv, int positio
 MVREXPORT void MvrArgumentBuilder::addStringsAsIs(int argc, char **argv, int position)
 {
   int i;
-  if(position<0)
+  if (position<0)
   {
     /// Don't try to use incremental positions, since the sequence would be out
     /// of order, just keep using the same special "at-end" meaning of negtive position
     /// value
-    for(i=0; i<argc; i++)
+    for (i=0; i<argc; i++)
       internalAddAsIs(argv[i], position);
    }
-  else
+  else 
   {
-    for(i=0; i<argc; i++)
+    for (i=0; i<argc; i++)
       internalAddAsIs(argv[i], position+i);
   }
 }
@@ -413,10 +413,273 @@ MVREXPORT void MvrArgumentBuilder::internalAddAsIs(const char *str, int position
 {
   size_t k = 0;
   bool addAtEnd;
-  if(position<0 || (size_t)position > myArgc)
+  if (position<0 || (size_t)position > myArgc)
     addAtEnd = true;
-  else
+  else 
     addAtEnd = false;
-  /// TODO
+  
+  if (addAtEnd)
+  {
+    myArgv[myArgc] = new char[strlen(str) + 1];
+    strcpy(myArgv[myArgc],str);
+    myArgv[myArgc][strlen(str)] = '\0';
 
+    /// add to our full string
+    /// if its not our first add a space
+    if (!myFirstAdd && myExtraSpace == '\0')
+      myFullString += " ";
+    else if (!myFirstAdd)
+      myFullString += myExtraSpace;
+    
+    myFullString += myArgv[myArgc];
+    myFirstAdd = false;
+
+    myArgc++;
+    myOrigArgc = myArgc;
+  }
+  else 
+  {
+    /// first move things down
+    for (k=myArgc+1; k>(size_t)position; k--)
+    {
+      myArgv[k] = myArgv[k-1];
+    }
+    myArgc++;
+    myOrigArgc = myArgc;
+
+    myArgv[position] = new char[strlen(str) + 1];
+    strcpy(myArgv[position], str);
+    myArgv[position][strlen(str)] = '\0';
+
+    rebuildFullString();
+    myFirstAdd = false;
+  }
+}
+
+MVREXPORT size_t MvrArgumentBuilder::getArgc(void) const
+{ return myArgc; }
+
+MVREXPORT char **MvrArgumentBuilder::getArgv(void) const
+{ return myArgv; }
+
+MVREXPORT const char *MvrArgumentBuilder::getFullString(void) const
+{ return myFullString.c_str(); }
+
+MVREXPORT const char *MvrArgumentBuilder::getExtraString(void) const
+{ return myExtraSting.c_str(); }
+
+MVREXPORT void MvrArgumentBuilder::setExtraString(const char *str)
+{ myExtraString = str; }
+
+MVREXPORT void MvrArgumentBuilder::setFullString(const char *str)
+{ myFullString = str; }
+
+MVREXPORT const char *MvrArgumentBuilder::getArg(size_t whichArg) const
+{
+  if ((whichArg > 0) && (whichArg < myArgc)){
+    return myArgv[whichArg];
+  }
+  else {
+    return NULL;
+  }
+}
+
+MVREXPORT void MvrArgumentBuilder::log(void) const
+{
+  size_t i;
+  MvrLog::log(MvrLog::Terse, "num arguments: %d", myArgc);
+  for (i=0; i<myArgc; ++i)
+    MvrLog::log(MvrLog::Terse, "Arg %d: %s", i, myArgv[i]);
+}
+
+MVREXPORT bool MvrArgumentBuilder::isArgBool(size_t whichArg) const
+{
+  if ((whichArg > myArgc) || getArg(whichArg) == NULL)
+    return NULL;
+  if (strcasecmp(getArg(whichArg), "true") == 0 ||
+      strcasecmp(getArg(whichArg), "1") == 0 |
+      strcasecmp(getArg(whichArg), "false") == 0 ||
+      strcasecmp(getArg(whichArg), "0") == 0 )
+    return true;
+  else
+    return false;
+}
+
+MVREXPORT bool MvrArgumentBuilder::getArgBool(size_t whichArg, bool *ok) const
+{
+  bool isSuccess = false;
+  bool ret = false;
+
+  const char *str = getArg(whichArg);
+
+  // If the arg was successfully obtained...
+  if (str != NULL){
+    if (strcasecmp(str, "true") == 0 || strcasecmp(str, "1") ==0){
+      isSuccess = true;
+      ret = true;
+    }
+    else if (strcasecmp(str, "false") == 0 || strcasecmp(str, "0") ==0){
+      isSuccess = false;
+      ret = false;
+    }
+  }
+
+  if (ok != NULL){
+    *ok = isSuccess;
+  }
+  if (isSuccess){
+    return ret;
+  }
+  else{
+    return 0;
+  }
+}
+
+MVREXPORT bool MvrArgumentBuilder::isArgInt(size_t whichArg, bool forceHex) const
+{
+  const char *str;
+  char *endPtr;
+  if (whichArg > myArgc || getArg(whichArg) == NULL)
+    return false;
+  
+  int base = 10;
+  str = getArg(whichArg);
+
+  if (forceHex)
+    base = 16;
+  /// See if it has the ehx prefix and strip it
+  if (strlen(str) > 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+  {
+    str = &str[2];
+    base = 16;
+  }
+  
+  strtol(str, &endPtr, base);
+  if (endPtr[0] == '\0' && endPtr != str)
+    return true;
+  else
+    return false;
+}
+
+MVREXPORT int MvrArgumentBuilder::getArgInt(size_t whichArg, bool *ok, bool forceHex) const
+{
+  bool isSuccess = false;
+  int ret = 0;
+
+  const char *str = getArg(whichArg);
+
+  /// If the specified arg was successfully obtained...
+  if(str != NULL){
+    int base = 10;
+    if (forceHex)
+      base = 16;
+    /// See if it has the hex prefix and strip it
+    if (strlen(str) > 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+     {
+       str = &str[2];
+       base = 16;
+      }
+    char *endPtr = NULL;
+    ret = strtol(str, &endPtr, base);
+
+    if (endPtr[0] == '\0' && endPtr != str){
+      isSuccess = true;
+    }
+  }
+
+  if (ok != NULL)
+    *ok = isSuccess;
+  if (isSuccess)
+    return ret;
+  else
+    return 0;
+}  // end method getArgInt
+
+MVREXPORT bool MvrArgumentBuilder::isArgLongLongInt(size_t whichArg) const
+{
+  const char *str;
+  char *endPtr;
+
+  if (whichArg > myArgc || getArg(whichArg) == NULL)
+    return false;
+  int base = 10;
+  str = getArg(whichArg);
+
+  /// See if it has the hex prefix and strip it
+  if (strlen(str) > 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+  {
+    str = &str[2];
+    base = 16;
+  }
+#ifndef _MSC_VER
+  strtoll(str, &endPtr, base);
+#else
+  strtoi64(str, &endPtr, base);
+#endif // _MSC_VER
+
+  if (endPtr[0] == '\0' && endPtr != str)
+    return true;
+  else
+    return false;
+}  // end method getArgLongLongInt
+
+MVREXPORT bool MvrArgumentBuilder::isArgDouble(size_t whichArg) const
+{
+  const char *str;
+  char *endPtr;
+  if (whichArg > myArgc || getArg(whichArg) == NULL)
+    return false;
+  
+  str = getArg(whichArg);
+
+  if (strcmp(str, "-INF") == 0)
+    return true;
+  else if (strcmp(str, "INF") == 0)
+    return true;
+  else
+  {
+    strtod(str, &endPtr);
+    if (endPtr[0] == '\0' && endPtr != str)
+      return true;
+    else
+      return false;
+  }
+}
+
+MVREXPORT double MvrArgumentBuilder::getArgDouble(size_t whichArg, bool *ok) const
+{
+  bool isSuccess = false;
+  double ret = 0;
+
+  const char *str = getArg(whichArg);
+
+  // If the specified arg was successfully obtained ..
+  if (str != NULL)
+  {
+    if (strcmp(str, "-INF") == 0)
+    {
+      isSuccess = true;
+      ret = -HUGE_VAL;
+    }
+    else if (strcmp(str, "INF") == 0)
+    {
+      isSuccess = true;
+      ret = HUGE_VAL;
+    }
+    else
+    {
+      char *endPtr = NULL;
+      ret = strtod(str, &endPtr);
+      if (endPtr[0] == '\0' && endPtr != str){
+        isSuccess = true;
+      }
+    }
+  }
+  if (ok != NULL){
+    *ok = isSuccess;
+  }
+  if (isSuccess)
+    return ret;
+  else 
+    return 0;
 }
