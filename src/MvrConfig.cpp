3a60d19d38f7aab70819b3075290a25ab35a363e
yjcs1990 <yjcs1990@163.com>
2017-05-31 22:40:23 +0800

    #add methods into class

--- src/MvrConfig.cpp
+++ src/MvrConfig.cpp
@@ -297,5 +297,375 @@ MVREXPORT MvrConfig &MvrConfig::Operator=(const MvrConfig &config)
 
 MVREXPORT void MvrConfig::copyAndDetach(const MvrConfig &config)
 {
-  
+  if (this != &config){
+    // Note than the name is not copied ...
+    IFDEBUG(MvrLog::log(MvrLog::Verbose,
+                        "%soperator=(from %s) begins",
+                        myLogPrefix.c_str(),
+                        config.myLogPrefix.c_str()));
+  // TODO: The following attributes also need to be copied (or
+  // something)
+  myArgumentParser = NULL;
+  setBaseDirectory(config.getBaseDirectory());
+  myNoBlanksBetweenParams = config.myNoBlanksBetweenParams;
+  myConfigVersion = config.myConfigVersion;
+  myIgnoreBounds = config.myIgnoreBounds;    
+  myFailOnBadSection = config.myFailOnBadSection;
+  mySection = config.mySection;
+  mySectionBroken = config.mySectionBroken;
+  mySectionIgnored = config.mySectionIgnored;
+  myUsingSections = config.myUsingSections;
+  myDuplicateParams = config.myDuplicateParams;
+
+  myIsParsingListBroken = config.myIsParsingListBroken;
+
+  myPermissionAllowFactory = config.myPermissionAllowFactory;
+  myPermissionSaveUnknown = config.myPermissionSaveUnknown;
+
+  myCategoryToSectionsMap.clear();
+  myCategoryToSectionsMap = config.myCategoryToSectionsMap;
+
+  clearSections();
+
+  stds::list<MvrConfigSection *>::const_iterator it;
+  for (it=config.mySections.begin(); it!=config.mySecitons.end(); it++)
+  {
+    MvrConfigSection *sectionCopy = new MvrConfigSection();
+    sectionCopy->setQuiet(myIsQuiet);
+    sectionCopy->copyAndDetach(*(*it));
+    // mySections.push_back(new MvrConfigSection(*(*it)));
+  }
+
+  copySectionsToParse(config.mySectionToParse);
+
+  myHighestPriorityToParse = config.myHighestPriorityToParse;
+  myLowestPriorityToParse  = config.myLowestPriorityToParse;
+  myRestartLevelNeeded     = config.myRestartLevelNeeded;
+  myCheckingForRestartLevel= config.myCheckingForRestartLevel;
+  mySectionsNotToParse     = config.mySectionsNotToParse;
+
+  remParserHandlers();
+  addParserHandlers();
+
+  IFDEBUG(MvrLog::log(MvrLog::Verbose,
+                      "%soperator=(from %s) ends",
+                      myLogPrefix.c_str(),
+                      config.myLogPrefix.c_str()));
+  }
+  // return *this;
+  }
 }
+/*
+ * These names are used solely for log messages.
+ * 
+ * @param configName a char * descriptive name of the ArConfig instance
+ * @param robotName an optional char * identifier of the robot that has 
+ * the config
+ */
+MVREXPORT void MvrConfig::setConfigName(const char *configName, const char *robotName)
+{
+  myConfigName = ((configName != NULL) ? configName : "");
+  myRobotName  = ((robotName != NULL) ? robotName : "");
+  myLogPrefix  = "";
+
+  if (!myRobotName.empty()){
+    myLogPrefix = myRobotName + ":";
+  }
+  myLogPrefix += "MvrConfig";
+  
+  if (!myConfigName.empyt()){
+    myLogPrefix += "("+myConfigName + ")";
+  }
+  myLogPrefix += ": ";
+}
+
+MVREXPORT void MvrConfig::setQuiet(bool isQuiet)
+{
+  myIsQuiet = isQuiet;
+  myParser.setQuiet(isQuiet);
+}
+
+MVREXPORT void MvrConfig::setTranslator(MvrConfig *xlatorConfig)
+{
+  myTranslator = xlatorConfig;
+
+  if (myTranslator != NULL){
+    if (!mySections.empty()){
+      MvrLog::log(MvrLog::Normal,
+                  "%sMvrConfig::setTranslator() sections already created",
+                  myLogPrefix.c_str());
+    for (std::list<MvrConfigSection *>::iterator iter = mySections.begin(); iter != mySections.end(); iter++){
+      translateSection(*iter);
+      }
+    }
+  }
+}
+
+MVREXPORT MvrConfig *MvrConfig::getTranslator() const
+{
+  return myTranslator;
+}
+
+/* 
+ * @param isSummary a bool set to true if only the section names and parameter counts
+ * should be logged; false to log detailed parameter information
+ * @param sectionNameList a list of the string section names to be logged; if NULL,
+ * then the data for all sections are logged
+ */
+MVREXPORT void MvrConfig::log(bool isSummary,
+                              std::list<std::string> *sectionNameList,
+                              const char *logPrefix)
+{
+  std::string origLogPrefix = myLogPrefix;
+
+  if (!MvrUtil::isStrEmpty(logPrefix)){
+    myLogPrefix = logPrefix;
+  }
+
+  std::list<MvrConfigArg> *params = NULL;
+
+  MvrLog::log(MvrLog::Normal, "%slog", myLogPrefix.c_str());
+
+  for (std::list<MvrConfigSection *>::const_iterator it = mySections.begin(); it != mySections.end(); it++)
+  {
+    params = (*it)->getParams();
+
+    // If the section names were specified and the current section isn't in the
+    // list, then skip the section...
+    if (sectionNameList != NULL){
+      if (!MvrUtil::isStrInList((*it)->getName(), *setctionNameList)){
+        continue;
+      }
+    }
+  
+    if (params == NULL){
+      ArLog::log(ArLog::Normal, "    Section %s has NULL params",
+                  (*it)->getName());
+      continue;
+    }
+
+    if (isSummary){
+      MvrLog::log(MvrLog::Normal,"    Section %s has %i params",
+                  (*it)->getName(), params->size());
+      continue;
+    }
+
+    MvrLog::log(MvrLog::Normal, "    Section %s:",
+                (*it)->getName());
+    for (std::list<MvrConfigArg>::iterator pit = params->begin(); pit != params->end(); pit++)
+    {
+      (*pit).log(false, 1, myLogPrefix.c_str());
+    }
+  } // end for loop
+
+  MvrLog::log(MvrLog::Normal, "%send",myLogPrefix.c_str());
+}                          
+
+MVREXPORT void MvrConfig::clearSections(void)
+{
+  IFDEBUG(MvrLog::log(MvrLog::Verbose, "%saclearSections() begin", myLogPrefix.c_str()));
+
+  while (mySections.begin() != mySections.end())
+  {
+    delete mySections.front();
+    mySections.pop_front();
+  }
+  // Clear this just in case ...
+  if(mySectionsToParse != NULL)
+  {
+    delete mySectionsToParse;
+    mySectionsToParse = NULL;
+  }
+
+  IFDEBUG(MvrLog::log(MvrLog::Verbose, "%sclearSections() end", myLogPrefix.c_str()));
+} 
+
+MVREXPORT void MvrConfig::clearAll(void)
+{
+  clearSections();
+  MvrUtil::deleteSetPairs(myProcessFileCBList.begin(), myProcessFileCBList.end());
+  myProcessFileCBList.clear();
+}   
+
+void MvrConfig::addParseHandlers(void)
+{
+  std::list<MvrConfigSection *>::const_iterator it;
+  std::list<MvrConfigArg> *param;
+  std::list<MvrConfigArg>::iterator it;
+
+  if (!myParser.addHandlerWithError(CONFIG_VERSION_TAG, &myVersionCB)){
+    if (!myIsQuiet){
+      MvrLog::log(MvrLog::Verbose, "%sCould not add ConfigVersion parser (probably unimportant)",
+                  myLogPrefix.c_str());
+    }
+  }
+  if (!myParser.addHandlerWithError("section", &mySectionCB)){
+    if (!myIsQuiet){
+      MvrLog::log(MvrLog::Verbose,
+                  "%sCould not add section parser (probably unimportant)",
+                  myLogPrefix.c_str());
+    }
+  }
+  if (!myParser.addHandlerWithError(MvrConfigArg::LIST_END_TAG, &myListEndCB)){
+    if (!myIsQuiet){
+      MvrLog::log(MvrLog::Verbose,
+                  "%sCould not add _listEnd parser",
+                  myLogPrefix.c_str());
+    }
+  }
+
+  for (it=mySections.begin(); it!=mySections.end(); it++)
+  {
+    params = (*it)->getParams();
+    if (params == NULL)
+      continue;
+    for (pit=params->begin(); pit!=params->end(); pit++)
+    {
+      (*pit).addToFileParser(&myParser, &myParserCB, myLogPrefix.c_str(), myIsQuiet);
+    }
+  }
+
+  if (!myParser.addHandlerWithError(NULL, &myUnknownCB)){
+    if (!myIsQuiet){
+      MvrLog::log(MvrLog::Verbose, "%sCould not add unknown param parser (probably unimportant)",
+                  myLogPrefix.c_str());
+    }
+  }
+}
+
+void MvrConfig::remParserHandlers(void)
+{
+  myParser.remHander(&myParserCB);
+  myParser.remHander(&myVersionCB);
+  myParser.remHander(&mySectionCB);
+  myParser.remHander(&myListBeginCB);
+  myParser.remHander(&myListEndCB);
+  myParser.remHander(&myUnknownCB);
+}
+
+MVREXPORT bool MvrConfig::addSection(const char *categoryName, const char *sectionName, const char *sectionDescription)
+{
+  if ((categoryName == NULL) || MvrUtil::isStrEmpty(sectionName)){
+    MvrLog::log(MvrLog::Normal, 
+                "%sFailed to add section '%s' to category '%s'",
+                myLogPrefix.c_str(),
+                ((sectionName != NULL) ? sectionName : "NULL"),
+                ((categoryName != NULL) ? categoryName : "NULL"));
+  return false;
+  }
+  MvrConfigSection *section = findSection(sectionName);
+  std::string origCategoryName;
+
+  if ((section != NULL) && (!MvrUtil::isStrEmpty(section->getCategoryName())))
+  {
+    translateSection(section);
+
+    MvrLog::log(MvrLog::Normal,
+                "%sFailed to add section '%s' to category '%s', already defined in category '%s'",
+                myLogPrefix.c_str(),
+                ((sectionName != NULL) ? sectionName : "NULL"),
+                ((categoryName != NULL) ? categoryName : "NULL"),
+                section->getCategoryName());
+    return false;
+  }
+
+  if (section == NULL){
+    MvrLog::log(MvrLog::Verbose, "%sMaking new section '%s' in category '%s'",
+                myLogPrefix.c_str(), sectionName, categoryName);
+
+    section = new MvrConfigSection(sectionName, sectionDescription, myIsQuiet, categoryName);
+    mySections.push_back(section);
+  }
+  else{
+    MvrLog::log(MvrLog::Verbose, "%sAssigning existing section '%s' to category '%s'",
+                myLogPrefix.c_str(), sectionName, categoryName);
+    origCategoryName = (section->getCategoryName() != NULL ? section->getCategoryName() : "");
+    section->setCategoryName(categoryName);
+    section->setComment(sectionDescription);     
+  }
+  translateSection(section);
+
+  // If the section was in a different category, then remove it
+  if (!origCategoryName.empty() && (MvrUtil::strcasecmp(origCategoryName, categoryName) != 0))
+  {
+    std::map<std::string, std::list<std::string> >::iterator origCatIter = myCategoryToSectionsMap.find(origCategoryName);
+    if (origCatIter != myCategoryToSectionsMap.end()){
+      std::list<std::string> *origSectionList = &(origCatIter->second);
+      if (origSectionList != NULL){
+        for (std::list<std::string>::iterator remIter=origSectionList->begin(); remIter!=origSectionList->end(); remIter++){
+          if (MvrUtil::strcasecmp(sectionName, *remIter) == 0){
+            origSectionList->erase(remIter);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // Make sure that section is not already stored in the category map
+  std::list<std::string> *sectionList = NULL;
+
+  std::map<std::string, std::list<std::string> >::iterator catIter=myCategoryToSectionsMap;
+  if (catIter != myCategoryToSectionsMap.end()){
+    sectionList = &(catIter->second);
+  }
+
+  bool isSectionFound = false;
+  if (sectionList != NULL){
+    isSectionFound = MvrUtil::isStrInList(sectionName, *sectionList);
+  }
+  // If the section is not already in the map, then add it
+  if (!isSectionFound){
+    myCategoryToSectionsMap[categoryName].push_back(sectionName);
+  }
+  return true;
+}
+
+/*
+ * Set the comment string associated with a section. If the section doesn't 
+ * exist then it is created.
+ * @deprecated use addSection instead
+ */
+MVREXPORT void MvrConfig::setSectionComment(const char *sectionName, const char *comment)
+{
+  MvrConfigSection *section = findSection(sectionName);
+
+  if (section == NULL)
+  {
+    MvrLog::log(MvrLog::Verbose, "%sMaking new section '%s' (for comment)",
+                myLogPrefix.c_str(), sectionName);
+    section = new MvrConfigSection(sectionName, comment, myIsQuiet);
+  
+    mySections.push_back(section);
+  }
+  else{
+    section->setComment(comment);
+  }
+  translateSection(section);
+}
+
+/*
+ * Add a flag to a section. If the section doesn't 
+ * exist then it is created.
+ * @warning The section name and flags must not contain any characters with
+ *          special meaning when saved and loaded from a config file, such as '#', ';',
+ *         tab, or newline.  
+ */
+MVREXPORT bool MvrConfig::addSectionFlags(const char *sectionName, const char *flags)
+{
+  MvrConfigSection *section = findSection(sectionName);
+
+  if (section == NULL)
+  {
+    MvrLog::log(MvrLog::Verbose, "%sMaking new section '%s' (flags)", myLogPrefix.c_str(), sectionName);
+    section = new MvrConfigSection(sectionName, NULL, myIsQuiet);
+    section->addFlags(flags, myIsQuiet);
+
+    translateSection(section);
+    mySections.push_back(section);
+  }
+  else
+    section->addFlags(flags, myIsQuiet);
+  return true;
+}
+
