594e32e654853c19e6be805c24ba0c0039eeedb4
yjcs1990 <yjcs1990@163.com>
2017-06-07 11:02:11 +0800

    @add methods into class

--- src/MvrThread.cpp
+++ src/MvrThread.cpp
@@ -35,3 +35,191 @@ MVREXPORT void MvrThread::stopAll()
   ourThreadsMutex.unlock();
 }
 
+MVREXPORT void MvrThread::joinAll()
+{
+  MapType::iterator iter;
+  MvrThread *thread = self();
+  ourThreadsMutex.lock();
+  for (iter=ourThreads.begin(); iter!=ourThreads.end(); ++iter)
+  {
+    if ((*iter).second->getJoinable() && thread && (thread != (*iter).second))
+    {
+      (*iter).second->doJoin();
+    }
+  }
+  ourThreads.clear();
+
+  if (thread != NULL){
+    addThreadToMap(thread->myThread, thread);
+  }
+  ourThreadsMutex.unlock();
+}
+
+MVREXPORT MvrThread::MvrThread(bool blockAllSignals) :
+          myName("(unnamed"),
+          myRunning(false),
+          myJoinable(false),
+          myBlockAllSignals(blockAllSignals),
+          myStarted(false),
+          myFinished(false),
+          myStrMap(),
+          myFunc(0),
+          myThread(),
+#if defined(WIN32) && !defined(MINGW)
+          myThreadHandle(0),
+#else
+          myPID(0),
+#endif          
+{
+}
+
+MVREXPORT MvrThread::MvrThread(MvrFunctor *func,
+                               bool joinable,
+                               bool blockAllSignals) :
+          myRunning(false),
+          myJoinable(false),
+          myBlockAllSignals(blockAllSignals),
+          myStarted(false),
+          myFinished(false),
+          myStrMap(),
+          myFunc(func),
+          myThread(),
+#if defined(WIN32) && !defined(MINGW)
+          myThreadHandle(0),
+#else
+          myPID(0),
+#endif 
+{
+  create(funct, joinable);
+}
+
+#if !defined(WIN32) || defined(MINGW)
+MVREXPORT MvrThread::~MvrThread()
+{
+  // Just make sure the thread is no longer in the map.
+  removeThreadFromMap(myThread);
+}
+#endif
+
+MVREXPORT int MvrThread::join(void **iret)
+{
+  int ret;
+  ret = doJoin(iret);
+  if (ret)
+    return (ret)
+  removeThreadFromMap(myThread);
+  return 0;
+}
+
+MVREXPORT void MvrThread::setThreadName(const char *name)
+{
+  myName = name;
+  std::string mutexLogName;
+  mutexLogName = name;
+  mutexLogName += "ThreadMutex";
+  myMutex.setLogName(mutexLogName.c_str());
+}
+
+MVREXPORT bool MvrThread::isThreadStarted() const
+{
+  return myStarted;
+}
+
+MVREXPORT bool MvrThread::isThreadFinished() const
+{
+  return myFinished;
+}
+
+MVREXPORT const char *MvrThread::getThisThreadName(const)
+{
+  MvrThread *self;
+  if ((self = MvrThread::self() != NULL))
+    return self->getThreadName();
+  else
+    return ourUnknownThreadName.c_str();
+}
+
+MVREXPORT const MvrThread::ThreadType *MvrThread::getThisThread(void)
+{
+  MvrThread *self;
+  if ((self = MvrThread::self() != NULL))
+    return self->getThread();
+  else
+    return NULL; 
+}
+
+MVREXPORT MvrThread::ThreadType MvrThread::getThisOSThread(void)
+{
+  MvrThread *self;
+  if ((self = MvrThread::self() != NULL))
+    return self->getOSThread();
+  else
+#ifdef MINGW
+  return (NULL, 0);
+#else
+  return 0;
+#endif
+}
+
+// ourThreads is a vector on MINGW and a map on Linux and Windows
+#ifdef MINGW
+MvrThread* MvrThread::findThreadInMap(ThreadType t) 
+{
+	ourThreadsMutex.lock();
+	for(MapType::iterator i = ourThreads.begin(); i != ourThreads.end(); ++i)
+	{
+		if(pthread_equal(t, (*i).first))
+		{
+			ourThreadsMutex.unlock();
+			return (*i).second;
+		}
+	}
+	ourThreadsMutex.unlock();
+	return NULL;
+}
+
+void MvrThread::removeThreadFromMap(ThreadType t) 
+{
+	ourThreadsMutex.lock();
+	MapType::iterator found = ourThreads.end();
+	for(MapType::iterator i = ourThreads.begin(); i != ourThreads.end(); ++i)
+		if(pthread_equal(t, (*i).first))
+			found = i;
+	if(found != ourThreads.end())
+		ourThreads.erase(found);
+	ourThreadsMutex.unlock();
+}
+
+void MvrThread::addThreadToMap(ThreadType pt, ArThread *at) 
+{
+	ourThreadsMutex.lock();
+	ourThreads.push_back(std::pair<ThreadType, ArThread*>(pt, at));
+	ourThreadsMutex.unlock();
+}
+#else
+MvrThread *MvrThread::findThreadInMap(ThreadType pt)
+{
+  ourThreadsMutex.lock();
+  MapType::iterator iter = ourThreads.find(pt);
+  MvrThread *r = NULL;
+  if (iter != ourThreads.end())
+    r = (*iter).second;
+  ourThreadsMutex.unlock();
+  return r;
+}
+void MvrThread::removeThreadFromMap(ThreadType t)
+{
+  ourThreadsMutex.lock();
+  MapType::iterator iter = ourThreads.find(t);
+  if (iter != ourThreads.end())
+    ourThreads.erase(iter);
+  ourThreadsMutex.unlock();
+}
+
+void MvrThread::addThreadToMap(ThreadType pt, MvrThread *at)
+{
+  ourThreadsMutex.lock();
+  ourThreads[pt] = at;
+  ourThreadsMutex.unlock();
+}
+#endif
